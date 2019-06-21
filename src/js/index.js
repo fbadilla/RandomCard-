ba2d98300a760125beecbcc94143ffc5bb384fb5
fbadilla <f.badilla228@gmail.com>
2019-06-21 20:06:20 +0000

    primera carga

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,18 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  number();
+  simbol();
 };
+
+function number() {
+  let excesarandom = numeros[Math.floor(Math.random() * 9)];
+  document.getElementById("number").innerHTML = excesarandom;
+}
+function simbol() {
+  let simbolrandom = simbolos[Math.floor(Math.random() * 3)];
+  document.getElementById("simbol").classList.add(simbolrandom);
+}
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let simbolos = ["heart", "spade", "club", "diamond"];
